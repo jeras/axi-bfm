dce30741abde1e7db9b822f0e0589c0c5164b810
Iztok Jeras <iztok.jeras@redpitaya.com>
2015-07-06 12:15:02 +0200

    cosmetic changes

--- sim/tb.sv
+++ sim/tb.sv
@@ -7,29 +7,22 @@ module tb #(
   parameter integer U = 1
 );
 
-  logic Clk;
-  logic Rst;
+  logic clk;
+  logic resetn;
   
-  initial       Clk = 1'b0;
-  always #(5ns) Clk = ~Clk;
+  initial       clk = 1'b0;
+  always #(5ns) clk = ~clk;
 
-  
-  reg [32-1:0] data;
-  reg  [2-1:0] resp;
+  logic [32-1:0] data;
+  logic  [2-1:0] resp;
   
   initial
   begin 
-    Rst = 1'b1; 
+    resetn = 1'b1; 
     #(2500ns)
-    @(posedge Clk);
-    Rst = 1'b0;
-  end
-  
-  initial
-  begin
-    // Wait for end of reset
-    @(negedge Rst);
-    @(posedge Clk);
+    @(posedge clk);
+    resetn = 1'b0;
+    @(posedge clk);
     Axi4Lite_M.WriteTransaction(32'h00000100, 3'b0, 32'h12345678, 4'b1011, resp);
     Axi4Lite_M.WriteTransaction(32'h12345678, 3'b0, 32'h0000abcd, 4'b1111, resp);
     Axi4Lite_M.ReadTransaction (32'h00000100, 3'b0, data                 , resp);
@@ -38,9 +31,9 @@ module tb #(
     Axi4Stream_M.SendRandomPacket(100);
   end
 
-  AXI4       #(.N(8), .I(1)) axi4       (.ACLK(Clk), .ARESETn(!Rst));
-  AXI4Lite   #(.N(4), .I(1)) axi4lite   (.ACLK(Clk), .ARESETn(!Rst));
-  AXI4Stream #(.N(4)       ) axi4stream (.ACLK(Clk), .ARESETn(!Rst));
+  AXI4             #(.N(8), .I(1)              ) axi4         (.ACLK(clk), .ARESETn(!resetn));
+  AXI4Lite         #(.N(4), .I(1)              ) axi4lite     (.ACLK(clk), .ARESETn(!resetn));
+  AXI4Stream       #(.N(4)                     ) axi4stream   (.ACLK(clk), .ARESETn(!resetn));
 
   Axi4LiteMaster   #(.N(4), .I(1)              ) Axi4Lite_M   (.intf(axi4lite)  );
   Axi4LiteSlave    #(.N(4), .I(1)              ) Axi4Lite_S   (.intf(axi4lite)  );
